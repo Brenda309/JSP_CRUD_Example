d0dfbcabc316d4720426f56985a409edea8b861c
Brenda Wihogora <brewihogora23@gmail.com>
2024-03-03 13:25:10 +0200

    Add getter and setter in the model.

--- src/main/java/com/brenda/jsp_crud_example/bean/User.java
+++ src/main/java/com/brenda/jsp_crud_example/bean/User.java
@@ -1,4 +1,67 @@
 package com.brenda.jsp_crud_example.bean;
 
 public class User {
+    private int id;
+    private String name,password,email,gender,country;
+
+    public User() {
+
+    }
+
+    public User(int id, String name, String password, String email, String gender, String country) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.gender = gender;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
